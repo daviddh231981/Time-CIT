4035a9fb06e99630d85f736ab3f9f76f329dff67
daviddh231981 <d231981@outlook.com>
2024-08-25 23:16:20 -0500

    lots of fixes.

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,24 @@ function showNav() {
       btnShowNavigation.innerHTML = "Hide";
   }
 }// JavaScript Document
+
+//Bcak To Top
+//Get the button:
+mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
